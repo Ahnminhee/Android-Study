aeb95f418e3a4bb55326d82ff698d5232624352b
Ahnminhee <sodndj115@daum.net>
2020-07-19 23:10:33 +0900

    study

--- openAPI/app/src/main/java/com/example/openapi/MainActivity.java
+++ openAPI/app/src/main/java/com/example/openapi/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String key = "";
+    private final String key = "l640r40BkQ%2Bmi6ZO9AvHYmCZZk%2BBVEBg%2BSG%2BKnR4Mn5MSa6Yv03XrQ2nlHVQST94XYFxPAn3rG2nExAqrm%2F2dg%3D%3D";
     private final String endPoint = "http://openapitraffic.daejeon.go.kr/api/rest/busposinfo/getBusPosByRtid";
 
     private EditText busNum_edit;
@@ -54,13 +54,6 @@ public class MainActivity extends AppCompatActivity {
         buffer = new StringBuffer();
     }
 
-
-    public void test() {
-        String test = endPoint + "?serviceKey=" + key + "&busRouteId=30300001&";
-        setUrlNParser(test);
-        showInfo_text.setText(buffer.toString());
-    }
-
     public void mOnClick(View v) {
         busNum = busNum_edit.getText().toString();
         station = station_edit.getText().toString();
@@ -69,9 +62,9 @@ public class MainActivity extends AppCompatActivity {
         buffer = new StringBuffer();
         showInfo_text.setText(" ");
 
-        /*if (exminData()) {
+        if (exminData()) {
             return;
-        }*/
+        }
 
         new Thread(new Runnable() {
             @Override
@@ -86,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         if (car1 == null) {
-                            buffer.append("도착 정보 x");
+                            buffer.append("도착 정보가 없습니다.");
                         } else {
                             buffer.append(" 차량 도착 정보");
                             buffer.append("\n차량 번호: " + car1);
@@ -115,11 +108,14 @@ public class MainActivity extends AppCompatActivity {
                     case XmlPullParser.START_TAG:
                         tag = xpp.getName();
 
-                        if (tag.equals("item")) ;
-                        else if (tag.equals("BUS_STOP_ID")) {
+                        if (tag.equals("BUs_NODE_ID")){
                             xpp.next();
                             buffer.append(xpp.getText());
 
+                        }
+                        else if (tag.equals("BUS_STOP_ID")) {
+                            xpp.next();
+                            buffer.append(xpp.getText());
                         } else if (tag.equals("bstopArsno")) ;
                         else if (tag.equals("bstopArsno")) ;
                         else if (tag.equals("bstopNm")) ;
@@ -229,23 +225,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean exminData() {
-        if (busNum.equals("") || station.equals("")) {
-            Toast.makeText(this, "값을 입력해주세여", Toast.LENGTH_SHORT).show();
+        /*if (busNum.equals("") || station.equals("")) {
+            Toast.makeText(this, "값을 입력해주세요", Toast.LENGTH_SHORT).show();
             return true;
         }
-
+*/
         String regExp = "([0-9])";
         Pattern pattern_symbol = Pattern.compile(regExp);
 
         Matcher matcher_busNum = pattern_symbol.matcher(busNum);
-        if (matcher_busNum.find() == false) {
-            Toast.makeText(this, "버스 번호 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+        /*if (matcher_busNum.find() == false) {
+            Toast.makeText(this, "버스 번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
             return true;
-        }
+        }*/
         Matcher matcher_station = pattern_symbol.matcher(station);
         if (matcher_station.find() == false) {
-            Toast.makeText(this, "정류장 번호 다시 입력해주세여", Toast.LENGTH_SHORT).show();
-            ;
+            Toast.makeText(this, "정류장 번호를 다시 입력해주세여", Toast.LENGTH_SHORT).show();
             return true;
         }
 
